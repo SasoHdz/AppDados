a9fa1886c496b6cfa1f70d026925c7b5343dc364
SasoHdz <isaacrdzcmd@gmail.com>
2023-02-22 20:29:35 -0600

    Firts version of dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados-celes';
+  dadoRigth: string= '../assets/dados/fase1.png';
+  dadoLefth: string = '../assets/dados/fase1.png';
+  sameFaces: boolean= false;
+  menssage: string = 'Sigue intentando!!!'
+
+  randomNumber() {
+   return Math.trunc(Math.random ()* 6 +1);
+  }
+
+  play(){
+       let nLefth:number = this.randomNumber();
+       let nRight:number = this.randomNumber();
+       this.dadoLefth = '../assets/dados/fase'+nLefth+'.png';
+       this.dadoRigth = '../assets/dados/fase'+nRight+'.png';
+
+       this.sameFaces = (nLefth===nRight);
+  }
+
+  random = this.randomNumber();
 }
